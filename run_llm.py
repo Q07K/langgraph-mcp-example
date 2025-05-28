7dce7d4a8b78bad0ecca305c70708582a08195fd
gyuhyeong <kgh0730@gmail.com>
2025-05-28 19:07:31 +0900

    Feat(llm): vLLM 서빙 모델 테스트용 코드 개선

--- run_llm.py
+++ run_llm.py
@@ -9,16 +9,40 @@ client = OpenAI(
     api_key="test",
 )
 
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "description": "Get current temperature for a given location.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "City and country e.g. Bogotá, Colombia",
+                    }
+                },
+                "required": ["location"],
+                "additionalProperties": False,
+            },
+        },
+    }
+]
+
 response = client.chat.completions.create(
     model="./Qwen/Qwen3-4B",
     messages=[
         # {"role": "system", "content": "/no_think"},
-        {"role": "user", "content": "안녕하세요."},
+        {"role": "user", "content": "오늘 서울 날씨가 어떄?"},
     ],
-    stream=True,
+    # stream=True,
+    tools=tools,
+    tool_choice="auto",
 )
+print(response)
 
-for chunk in response:
-    content = chunk.choices[0].delta.content
-    if content is not None:
-        print(content, end="", flush=True)
+# for chunk in response:
+#     content = chunk.choices[0].delta.content
+#     if content is not None:
+#         print(content, end="", flush=True)

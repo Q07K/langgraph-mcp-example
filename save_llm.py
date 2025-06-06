2b185f52b6810f4ab7a63051f7c4c1a7094d8a91
gyuhyeong <kgh0730@gmail.com>
2025-06-06 17:29:57 +0900

    Feat(llm): huggingface_hub를 활용한 llm 저장 스크립트 코드

---
+++ save_llm.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+from huggingface_hub import login, snapshot_download
+
+load_dotenv()
+
+login(token=os.getenv("HUGGINGFACE_TOKEN"))
+
+
+repo_id = "Qwen/Qwen3-4B"
+
+model_path = snapshot_download(
+    repo_id=repo_id, local_dir="llm/" + repo_id + "/"
+)
+print(f"Model downloaded to {model_path}")

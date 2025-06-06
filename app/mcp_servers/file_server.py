520bc8d5b403edc13c0493b4ac90f854b0e29323
gyuhyeong <kgh0730@gmail.com>
2025-06-06 18:16:30 +0900

    Feat(mcp-serever): 테스트용 custom mcp server 구현
    - **file_server:** work space 내 파일 구조를 조회할 수 있는 `stdio` server
    - **math_server:** 사칙 연산을 할 수 있는 `sse` server
    - **time_server:** 사용자 os 시간을 확인할 수 있는 `sse` server

---
+++ app/mcp_servers/file_server.py
@@ -0,0 +1,60 @@
+# math_server.py
+import os
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(name="FileManager")
+
+
+@mcp.tool()
+def list_files(directory: str = ".", limit: int = 10) -> str:
+    """디렉토리의 파일 목록을 가져옵니다
+
+    Parameters
+    ----------
+    directory : str, optional
+        조회할 디렉토리 경로, by default "."
+    limit : int, optional
+        조회할 디렉토리 내부 파일 겟수, by default 10
+
+    Returns
+    -------
+    str
+        파일 목록 문자열
+    """
+
+    try:
+        files = os.listdir(directory)
+
+        file_list = "\n".join(files[:limit])
+
+        return f"{directory} 디렉토리 파일 목록:\n{file_list}"
+    except Exception as e:
+        return f"오류: {e}"
+
+
+@mcp.tool()
+def file_info(filename: str) -> str:
+    """파일 정보를 가져옵니다
+
+    Parameters
+    ----------
+    filename : str
+        정보를 확인할 파일명
+
+    Returns
+    -------
+    str
+        파일 정보 문자열
+    """
+    if os.path.exists(filename):
+        size = os.path.getsize(filename)
+        return f"{filename}: 크기 {size} bytes"
+
+    return f"{filename}: 파일이 존재하지 않습니다."
+
+
+if __name__ == "__main__":
+    # stdio 전송을 사용하여 서버 실행
+    print("Starting Math MCP server via stdio...")
+    mcp.run(transport="stdio")
---
+++ app/mcp_servers/time_server.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from mcp.server.fastmcp import FastMCP
+
+# Initialize FastMCP server with configuration
+mcp = FastMCP(
+    name="time",  # Name of the MCP server
+    instructions="You are a time assistant that can provide the current time",  # Instructions for the LLM on how to use this tool
+    host="0.0.0.0",  # Host address (0.0.0.0 allows connections from any IP)
+    port=8005,  # Port number for the server
+)
+
+
+@mcp.tool()
+async def get_time() -> str:
+    """
+    Get current time in user os".
+
+    Returns:
+        str: A string containing the current time information for the specified timezone
+    """
+    current_time = datetime.now()
+
+    # Format the time as a string
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+    return f"Current time is: {formatted_time}"
+
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")

6a7d88dbafc45438f519e95cb9088e1e4e6db089
gyuhyeong <kgh0730@gmail.com>
2025-06-06 17:36:48 +0900

    Feat(model): LangChain 기반 LLM message 포맷화 모듈

---
+++ app/models/chat_model.py
@@ -0,0 +1,55 @@
+def user_message(content: str) -> dict[str, str]:
+    """User Message를 나타내는 딕셔너리 생성
+
+    Parameters
+    ----------
+    content : str
+        메시지 내용
+
+    Returns
+    -------
+    dict[str, str]
+        주어진 내용을 포함하는 딕셔너리
+    """
+    return {
+        "role": "user",
+        "content": content,
+    }
+
+
+def system_message(content: str) -> dict[str, str]:
+    """System Message를 나타내는 딕셔너리 생성
+
+    Parameters
+    ----------
+    content : str
+        메시지 내용
+
+    Returns
+    -------
+    dict[str, str]
+        주어진 내용을 포함하는 딕셔너리
+    """
+    return {
+        "role": "system",
+        "content": content,
+    }
+
+
+def assistant_message(content: str) -> dict[str, str]:
+    """Assistant Message를 나타내는 딕셔너리 생성
+
+    Parameters
+    ----------
+    content : str
+        메시지 내용
+
+    Returns
+    -------
+    dict[str, str]
+        주어진 내용을 포함하는 딕셔너리
+    """
+    return {
+        "role": "assistant",
+        "content": content,
+    }
